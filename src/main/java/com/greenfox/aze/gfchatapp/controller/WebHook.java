4e3edd2a5ff5e2d766c33ac52ba76f4b3a8a8207
aze <adam.gyulavari@lab.coop>
2017-11-06 16:11:32 +0100

    change verify

--- src/main/java/com/greenfox/aze/gfchatapp/controller/WebHook.java
+++ src/main/java/com/greenfox/aze/gfchatapp/controller/WebHook.java
@@ -2,13 +2,15 @@ package com.greenfox.aze.gfchatapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/webhook")
 public class WebHook {
+
     @GetMapping({"/", ""})
-    public String receiveEvent() {
-        return "1943161557";
+    public String receiveEvent(@RequestParam("hub.challenge") String challenge) {
+        return challenge;
     }
 }
