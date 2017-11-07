da2e2d0fd73820d9545c2762d6eb8538217a9a5e
aze <adam.gyulavari@lab.coop>
2017-11-07 12:07:20 +0100

    try6

--- src/main/java/com/greenfox/aze/gfchatapp/model/Message.java
+++ src/main/java/com/greenfox/aze/gfchatapp/model/Message.java
@@ -1,5 +1,8 @@
 package com.greenfox.aze.gfchatapp.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Message {
     public String mid;
     public String text;
--- src/main/java/com/greenfox/aze/gfchatapp/model/Messaging.java
+++ src/main/java/com/greenfox/aze/gfchatapp/model/Messaging.java
@@ -1,5 +1,8 @@
 package com.greenfox.aze.gfchatapp.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Messaging {
     public User sender;
     public User recipient;
